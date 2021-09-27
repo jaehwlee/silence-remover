5b45131d2c1979fed04ce2e09c2be4d6924b65e5
Jaehwan Lee <jaehwlee@gmail.com>
2021-09-27 17:53:30 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 from utils import remove_silence
 from utils import load_audio
 
+
 if __name__ == "__main__":
-	PATH = '/some/where/file.wav'
-	audio = load_audio(PATH)
-	trimmed_audio = remove_silence(audio)
+    PATH = '/some/where/file.wav'
+    audio = load_audio(PATH)
+    trimmed_audio = remove_silence(audio)
